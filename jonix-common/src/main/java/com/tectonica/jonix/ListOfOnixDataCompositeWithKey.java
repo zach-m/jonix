c81295842e794c5b75c4e8fa414adaf87f387f33
Zach Melamed <zach@tectonica.co.il>
2019-05-27 11:04:17 +0300

    (minor)

--- jonix-common/src/main/java/com/tectonica/jonix/ListOfOnixDataCompositeWithKey.java
+++ jonix-common/src/main/java/com/tectonica/jonix/ListOfOnixDataCompositeWithKey.java
@@ -21,7 +21,6 @@ package com.tectonica.jonix;
 
 import com.tectonica.jonix.OnixComposite.OnixDataCompositeWithKey;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
