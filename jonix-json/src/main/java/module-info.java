b882d289940899a6f063b2125014c5bba2ef8a96
Zach Melamed <zach@tectonica.co.il>
2020-05-16 11:56:33 +0300

    adapted to jackson 2.9.10.4

--- jonix-json/src/main/java/module-info.java
+++ jonix-json/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module jonix.json {
     requires transitive jonix.common;
 
-    requires jackson.annotations;
+    requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
 
