16bb7d63387d8d5176af99f8f4249df254fb4733
Zach Melamed <zach@tectonica.co.il>
2023-01-31 18:30:25 +0200

    updated copyright line to 2023

--- jonix-onix2/src/main/java/com/tectonica/jonix/onix2/PriceEffectiveUntil.java
+++ jonix-onix2/src/main/java/com/tectonica/jonix/onix2/PriceEffectiveUntil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2022 Zach Melamed
+ * Copyright (C) 2012-2023 Zach Melamed
  *
  * Latest version available online at https://github.com/zach-m/jonix
  * Contact me at zach@tectonica.co.il
