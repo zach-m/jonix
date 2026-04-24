f3e0e4a24b5078adf04fb4e1363e372bb6814653
Zach Melamed <zach@tectonica.co.il>
2026-04-24 11:47:28 +0300

    smart pageCount selector for Onix3

--- jonix/src/main/java/com/tectonica/jonix/unify/base/onix3/BaseDescription3.java
+++ jonix/src/main/java/com/tectonica/jonix/unify/base/onix3/BaseDescription3.java
@@ -19,14 +19,16 @@
 
 package com.tectonica.jonix.unify.base.onix3;
 
-import com.tectonica.jonix.common.codelist.ExtentTypes;
+import com.tectonica.jonix.common.codelist.ExtentUnits;
 import com.tectonica.jonix.common.codelist.Languages;
 import com.tectonica.jonix.common.codelist.ProductForms;
-import com.tectonica.jonix.common.struct.JonixExtent;
 import com.tectonica.jonix.onix3.AudienceRange;
 import com.tectonica.jonix.onix3.DescriptiveDetail;
 import com.tectonica.jonix.onix3.Product;
 import com.tectonica.jonix.unify.base.BaseDescription;
+
+import java.util.Comparator;
+import java.util.Optional;
 
 /**
  * ONIX3 concrete implementation for {@link BaseDescription}
@@ -47,16 +49,26 @@ public class BaseDescription3 extends BaseDescription {
             dest.editionNumber = dd.editionNumber().value;
             ProductForms productFormValue = dd.productForm().value;
             dest.productForm = (productFormValue == null) ? null : productFormValue.description;
-            JonixExtent jNumberOfPages = dd.extents().findAsStruct(ExtentTypes.Main_content_page_count).orElse(null);
-            dest.numberOfPages = (jNumberOfPages == null || jNumberOfPages.extentValue == null) ? null
-                : jNumberOfPages.extentValue.toString();
+            dest.numberOfPages = pageCount(dd).map(Object::toString).orElse(null);
             dest.languages = dd.languages().asStructs();
             dest.audiences = dd.audiences().asStructs();
-            dest.audienceDescription = dd.audienceDescriptions().stream()
-                .filter(ad -> ad.language == null || ad.language == Languages.English)
-                .map(ad -> ad.value)
+            dest.audienceDescription = dd.audienceDescriptions().stream() //
+                .filter(ad -> ad.language == null || ad.language == Languages.English) //
+                .map(ad -> ad.value) //
                 .findFirst().orElse(null);
             dest.audienceRange = dd.audienceRanges().first().map(AudienceRange::asStruct).orElse(null);
         }
+    }
+
+    public static Optional<Integer> pageCount(DescriptiveDetail dd) {
+        return dd.extents().stream()
+            // pick only Extents with ExtentUnit of "Pages"
+            .filter(e -> e.extentUnit().value().map(u -> u == ExtentUnits.Pages).orElse(false))
+            // of those, take the one with the lowest (more primary) ExtentType value (e.g. prefer "00" over "06")
+            .min(Comparator.comparingInt(e -> e.extentType().value().map(Enum::ordinal).orElse(Integer.MAX_VALUE)))
+            // return the ExtentValue, which is defined as Double for the general case
+            .flatMap(e -> e.extentValue().value())
+            // As we've taken a unit of type "Pages", we expect it to be an integer
+            .map(Double::intValue);
     }
 }
